1be6ba10767077d687f2b71ef6017aa64dd79bea
Md Noman <md3173505@gmail.com>
2026-08-18 13:22:21 +0600

    Create versel.js for Vercel deployment
    Add Vercel configuration for serverless function

---
+++ versel.js
@@ -0,0 +1,20 @@
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "api/bot.js",
+      "use": "@vercel/node"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "api/bot.js"
+    }
+  ],
+  "env": {
+    "BOT_TOKEN": "8240762466:AAGFwt6bVA9cJDfeOOtS6Eb3Q6lF85kb5w4",
+    "ADMIN_ID": "8128648817",
+    "VPS_API": "http://66.33.22.220:3000"
+  }
+    }
